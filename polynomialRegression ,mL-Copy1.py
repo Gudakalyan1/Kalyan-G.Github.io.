beb222bf9bafcfb43a90bc0f5088c769c3e5dd7f
Kalyan G <81285342+Gudakalyan1@users.noreply.github.com>
2021-05-31 08:15:25 +0530

    Add files via upload

---
+++ polynomialRegression ,mL-Copy1.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[65]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+import sklearn.metrics as metrics
+import seaborn as sns
+
+
+# In[2]:
+
+
+df=pd.read_csv("Downloads\Hr data.csv")
+
+
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+df.corr()
+
+
+# In[22]:
+
+
+x=df.iloc[:,1:2].values
+y=df.iloc[:,2].values
+x
+
+
+# In[6]:
+
+
+#checking for linear relation
+plt.scatter(x,y)
+plt.ylim(0,)
+plt.show()
+
+
+# In[7]:
+
+
+#splitting data set into train set and test split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+x_train,x_test,y_train,y_test
+                            
+
+
+# In[8]:
+
+
+#fitting linear regression to the data set
+reg=linear_model.LinearRegression()
+reg.fit(x,y)
+reg.predict(x)
+
+
+# In[9]:
+
+
+#visualizing the results
+plt.scatter(x,y,color='red')
+plt.plot(x,reg.predict(x),color='blue')
+plt.show()
+
+
+# In[48]:
+
+
+#fitting polynomial regression to the data set
+poly_reg=PolynomialFeatures(degree=4)
+x_poly=poly_reg.fit_transform(x)
+x_poly.reshape(1,-1)
+
+
+# In[62]:
+
+
+pr=linear_model.LinearRegression()
+pr.fit(x_poly,y)
+p=pr.predict(x_poly)
+
+
+# In[12]:
+
+
+#visualizing the results
+plt.scatter(x,y)
+plt.plot(x,pr.predict(x_poly))
+plt.show()
+
+
+# In[13]:
+
+
+reg.predict([[5.5]])
+
+
+# In[14]:
+
+
+#metrics calculation
+mse=metrics.mean_squared_error(y,p)
+r2=metrics.r2_score(y,p)
+print(mse,r2)
+
+
+# In[63]:
+
+
+pr.predict(poly_reg.fit_transform([[5.5]]))
+
+
+# In[64]:
+
+
+pr.predict(poly_reg.fit_transform([[6.7]]))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
